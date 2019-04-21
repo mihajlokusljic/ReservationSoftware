5e6c2fa87c7fd3450bd908c6e00cec15eed3d8c2
Mihajlo Kusljic <mihajlokusljic97@gmail.com>
2019-04-21 17:51:30 +0200

    Dodati DTO objekti za pretragu hotela

---
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/dto/PotrebnoSobaDTO.java
@@ -0,0 +1,22 @@
+package rs.ac.uns.ftn.isa9.tim8.dto;
+
+public class PotrebnoSobaDTO {
+	protected int brKrebetaPoSobi;
+	protected int brSoba;
+	public PotrebnoSobaDTO() {
+		super();
+	}
+	public int getBrKrebetaPoSobi() {
+		return brKrebetaPoSobi;
+	}
+	public void setBrKrebetaPoSobi(int brKrebetaPoSobi) {
+		this.brKrebetaPoSobi = brKrebetaPoSobi;
+	}
+	public int getBrSoba() {
+		return brSoba;
+	}
+	public void setBrSoba(int brSoba) {
+		this.brSoba = brSoba;
+	}
+	
+}
---
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/repository/RezervacijeSobaRepository.java
@@ -0,0 +1,9 @@
+package rs.ac.uns.ftn.isa9.tim8.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rs.ac.uns.ftn.isa9.tim8.model.RezervacijaSobe;
+
+public interface RezervacijeSobaRepository extends JpaRepository<RezervacijaSobe, Long> {
+
+}
