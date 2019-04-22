d6a125611284a2e203eb9aba7e8adf445d6af89d
Nikolina Batinic <nina.batinic@yahoo.com>
2019-04-22 23:46:58 +0200

    Implementirano propagiranje informacija pri logovanju administratora rent-a-car sistema i izmjenjen front stranice rent-a-car sistema

--- src/main/java/rs/ac/uns/ftn/isa9/tim8/dto/KorisnikDTO.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/dto/KorisnikDTO.java
@@ -1,27 +1,57 @@
 package rs.ac.uns.ftn.isa9.tim8.dto;
 
+import rs.ac.uns.ftn.isa9.tim8.model.Adresa;
+
 public class KorisnikDTO {
 	
-	protected String username;
+	protected String ime;
+	
+	protected String prezime;
+	
+	protected String email;
 	
 	protected String lozinka;
+
+	protected String brojTelefona;
+	
+	protected Adresa adresa;
 	
 	public KorisnikDTO() {
 		super();
 	}
 
-	public KorisnikDTO(String username, String lozinka) {
+	public KorisnikDTO(String ime, String prezime, String email, String lozinka, String brojTelefona, Adresa adresa) {
 		super();
-		this.username = username;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.email = email;
 		this.lozinka = lozinka;
+		this.brojTelefona = brojTelefona;
+		this.adresa = adresa;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getIme() {
+		return ime;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getLozinka() {
@@ -31,7 +61,21 @@ public class KorisnikDTO {
 	public void setLozinka(String lozinka) {
 		this.lozinka = lozinka;
 	}
-	
-	
+
+	public String getBrojTelefona() {
+		return brojTelefona;
+	}
+
+	public void setBrojTelefona(String brojTelefona) {
+		this.brojTelefona = brojTelefona;
+	}
+
+	public Adresa getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(Adresa adresa) {
+		this.adresa = adresa;
+	}
 	
 }
