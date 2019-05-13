a2843aeb062695d3108269fbd7dc63cf94d29a28
Mihajlo Kusljic <mihajlokusljic97@gmail.com>
2019-05-13 19:31:37 +0200

    Omogucen prikaz podataka o aviokompaniji, njenim destinacijama, avionima, letovima i prtljagu na info stranici

--- src/main/java/rs/ac/uns/ftn/isa9/tim8/controller/LetoviKontroler.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/controller/LetoviKontroler.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +28,15 @@ public class LetoviKontroler {
 	@RequestMapping(value = "/dobaviSve", method = RequestMethod.GET)
 	public ResponseEntity<Collection<Let>> dobaviLetove() {
 		return new ResponseEntity<Collection<Let>>(servis.dobaviLetove(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/letoviAviokompanije/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> dobaviLetoveAviokompanije(@PathVariable("id") Long aviokompanijaId) {
+		try {
+			return new ResponseEntity<Collection<Let> >(servis.dobaviLetoveAviokompanije(aviokompanijaId), HttpStatus.OK);
+		} catch (NevalidniPodaciException e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
 
 	@RequestMapping(value = "/dodaj", method = RequestMethod.POST)
---
+++ src/main/webapp/infoStranicaAviokompanije/js/aviokompanijaInfo.js
@@ -0,0 +1,204 @@
+let podaciAviokompanije = null;
+let defaultSlika = "https://cdn.logojoy.com/wp-content/uploads/2018/05/30142202/1_big-768x591.jpg";
+let stavkeMenija = ["info", "pregledaj", "brzeRezervacije"];
+let tabovi = ["tab-info", "tab-destinacije", "tab-avioni", "tab-letovi", "tab-prtljag", "tab-brze-rezervacije"];
+
+
+$(document).ready(function(e) {
+	
+	//dodavanje zaglavlja sa JWT tokenom u svaki zahtjev upucen ajax pozivom i obrada gresaka
+	$.ajaxSetup({
+	    headers: createAuthorizationTokenHeader(),
+	    error: function(XMLHttpRequest, textStatus, errorThrown) {
+	    	let statusCode = XMLHttpRequest.status;
+	    	if(statusCode == 400) {
+	    		//u slucaju neispravnih podataka (Bad request - 400) prikazuje se
+	    		//poruka o greski koju je server poslao
+	    		alert(XMLHttpRequest.responseText);
+	    	}
+	    	else {
+	    		alert("AJAX error - " + XMLHttpRequest.status + " " + XMLHttpRequest.statusText + ": " + errorThrown);
+	    	}
+		}
+	});
+	
+	$("#info").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("info");
+		prikaziTab("tab-info");
+	});
+	
+	$("#destinacije").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("pregledaj");
+		prikaziTab("tab-destinacije");
+	});
+	
+	$("#avioni").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("pregledaj");
+		prikaziTab("tab-avioni");
+	});
+	
+	$("#letovi").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("pregledaj");
+		prikaziTab("tab-letovi");
+	});
+	
+	$("#prtljag").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("pregledaj");
+		prikaziTab("tab-prtljag");
+	});
+	
+	$("#brzeRezervacije").click(function(e) {
+		e.preventDefault();
+		aktivirajStavkuMenija("brzeRezervacije");
+		prikaziTab("tab-brze-rezervacije");
+	});
+	
+	
+	ucitajPodatkeAviokompanije();
+});
+
+function prikaziTab(idTaba) {
+	for(i in tabovi) {
+		if(idTaba == tabovi[i]) {
+			$("#" + tabovi[i]).addClass("active");
+		} else {
+			$("#" + tabovi[i]).removeClass("active");
+		}
+	}
+}
+
+function aktivirajStavkuMenija(idStavke) {
+	for(i in stavkeMenija) {
+		if(idStavke == stavkeMenija[i]) {
+			$("#" + stavkeMenija[i]).addClass("active");
+		} else {
+			$("#" + stavkeMenija[i]).removeClass("active");
+		}
+	}
+}
+
+function ucitajPodatkeAviokompanije() {
+	var url = window.location.href;
+	var parametri = url.substring(url.indexOf("?") + 1);
+	var params_parser = new URLSearchParams(parametri);
+	
+	var id = params_parser.get("id");
+	
+	$.ajax({
+		type: "GET",
+		url: "../aviokompanije/dobavi/" + id,
+		success: function(response) {
+			podaciAviokompanije = response;
+			prikaziPodatkeAviokompanije();
+			prikaziDestinacije();
+			prikaziAvione();
+			ucitajLetove();
+			prikaziCjenovnikPrtljaga();
+			
+		},
+	});
+
+}
+
+function prikaziPodatkeAviokompanije() {
+	$("#slikaAviokompanije").attr("src", defaultSlika);
+	$("#title").html(podaciAviokompanije.naziv);
+	$("#aviokompanijaNaslov").html(podaciAviokompanije.naziv);
+	$("#nazivAviokompanije").val(podaciAviokompanije.naziv);
+	$("#adresaAviokompanije").val(podaciAviokompanije.adresa.punaAdresa);
+	let sumaOcjena = podaciAviokompanije.sumaOcjena;
+	sumaOcjena = parseFloat(sumaOcjena);
+	let brOcjena = podaciAviokompanije.brojOcjena;
+	brOcjena = parseInt(brOcjena);
+	if(brOcjena > 0) {
+		$("#ocjenaAviokompanije").val(sumaOcjena / brOcjena);
+	} else {
+		$("#ocjenaAviokompanije").val("Nema ocjena");
+	}
+	$("#opisAviokompanije").val(podaciAviokompanije.promotivniOpis);
+}
+
+function prikaziDestinacije() {
+	let tabela = $("#prikazDestinacija");
+	tabela.empty();
+	
+	$.each(podaciAviokompanije.destinacije, function(i, destinacija) {
+		let noviRed = $("<tr></tr>");
+		noviRed.append('<td class="column3"><img src="' + destinacija.putanjaSlike + '" alt="Greska pri učitavanju slike"/></td>');
+		noviRed.append('<td class="column3">' + destinacija.nazivDestinacije + '</td>');
+		noviRed.append('<td class="column3">' + destinacija.adresa.punaAdresa + '</td>');
+		tabela.append(noviRed);
+	});
+}
+
+function prikaziAvione() {
+	let tabela = $("#prikazAviona");
+	tabela.empty();
+	
+	$.each(podaciAviokompanije.avioni, function(i, avion) {
+		let noviRed = $("<tr></tr>");
+		noviRed.append('<td class="column3"><img src="' + avion.putanjaSlike + '" alt="Greska pri učitavanju slike"/></td>');
+		noviRed.append('<td class="column3">' + avion.naziv + '</td>');
+		noviRed.append('<td class="column5">' + avion.sjedista.length + '</td>');
+		noviRed.append('<td class="column5">' + avion.segmenti.length + '</td>');
+		tabela.append(noviRed);
+	});
+}
+
+function ucitajLetove() {
+	
+	$.ajax({
+		type: "GET",
+		url: "../letovi/letoviAviokompanije/" + podaciAviokompanije.id,
+		success: function(response) {
+			prikaziLetove(response);
+		},
+	});
+	
+}
+
+function prikaziLetove(letovi) {
+	let tabela = $("#prikazLetova");
+	tabela.empty();
+	
+	$.each(letovi, function(i, tekuciLet) {
+		let noviRed = $("<tr></tr>");
+		
+		let datum_poletanja = new Date(tekuciLet.datumPoletanja);
+		let datum_sletanja = new Date(tekuciLet.datumSletanja);
+		
+		noviRed.append('<td class="column2">' + tekuciLet.brojLeta + "</td>");
+		noviRed.append('<td class="column3">' + tekuciLet.polaziste.nazivDestinacije + "</td>");
+		noviRed.append('<td class="column3">' + tekuciLet.odrediste.nazivDestinacije + "</td>");
+		noviRed.append('<td class="column1">' + datum_poletanja.getFullYear() + "-" + datum_poletanja.getMonth() + 
+				"-" + datum_poletanja.getDate() + "</td>");
+		noviRed.append('<td class="column1">' + datum_sletanja.getFullYear() + "-" + datum_sletanja.getMonth() + 
+				"-" + datum_sletanja.getDate() + "</td>");
+		noviRed.append('<td class="column5">' + tekuciLet.presjedanja.length + "</td>");
+		noviRed.append('<td class="column6">' + tekuciLet.cijenaKarte + "</td>");
+		if (tekuciLet.brojOcjena > 0) {
+			noviRed.append('<td class="column6">' + (tekuciLet.sumaOcjena / tekuciLet.brojOcjena) + "</td>");
+		} else {
+			noviRed.append('<td class="column6">Nema ocjena</td>');
+		}
+		tabela.append(noviRed);
+	});
+}
+
+function prikaziCjenovnikPrtljaga() {
+	let tabela = $("#prikazPrtljaga");
+	tabela.empty();
+	
+	$.each(podaciAviokompanije.cjenovnikDodatnihUsluga, function(i, prtljag) {
+		let noviRed = $("<tr></tr>");
+		noviRed.append('<td class="column1">' + prtljag.naziv + '</td>');
+		noviRed.append('<td class="column1">' + prtljag.opis + '</td>');
+		noviRed.append('<td class="column1">' + prtljag.cijena + '</td>');
+		tabela.append(noviRed);
+	});
+}
