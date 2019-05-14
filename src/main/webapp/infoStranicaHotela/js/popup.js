cae7901d5323b6aecb1a7e0dc08b7a418d8b6f3b
Mihajlo Kusljic <mihajlokusljic97@gmail.com>
2019-05-15 01:02:10 +0200

    Dodat popup window za mapu na info stranici hotela

---
+++ src/main/webapp/infoStranicaHotela/js/popup.js
@@ -0,0 +1,11 @@
+$(document).ready(function (e) {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
