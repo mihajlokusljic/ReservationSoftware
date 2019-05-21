5ae32da118ea18c2aaeb3872ecd6f115c981a8e8
Nikola Zubic <nikolazubic97@yahoo.com>
2019-05-21 04:16:32 +0200

    Dodat DTO za brzu rezervaciju karte, funkcija za dodavanje,izmj. model

--- src/main/java/rs/ac/uns/ftn/isa9/tim8/dto/BrzaRezervacijaKarteDTO.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/dto/BrzaRezervacijaKarteDTO.java
@@ -1,5 +1,104 @@
 package rs.ac.uns.ftn.isa9.tim8.dto;
 
+import java.util.Date;
+
 public class BrzaRezervacijaKarteDTO {
+
+	protected Long id;
+
+	protected Long letId;
+
+	protected Long aviokompanijaId;
+
+	protected Long sjedisteId;
+
+	protected Date datumPolaska;
+
+	protected Date datumDolaska;
+
+	protected double cijena;
+
+	protected int procenatPopusta;
+
+	public BrzaRezervacijaKarteDTO() {
+		super();
+	}
+
+	public BrzaRezervacijaKarteDTO(Long id, Long letId, Long aviokompanijaId, Long sjedisteId, Date datumPolaska,
+			Date datumDolaska, double cijena, int procenatPopusta) {
+		super();
+		this.id = id;
+		this.letId = letId;
+		this.aviokompanijaId = aviokompanijaId;
+		this.sjedisteId = sjedisteId;
+		this.datumPolaska = datumPolaska;
+		this.datumDolaska = datumDolaska;
+		this.cijena = cijena;
+		this.procenatPopusta = procenatPopusta;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getLetId() {
+		return letId;
+	}
+
+	public void setLetId(Long letId) {
+		this.letId = letId;
+	}
+
+	public Long getAviokompanijaId() {
+		return aviokompanijaId;
+	}
+
+	public void setAviokompanijaId(Long aviokompanijaId) {
+		this.aviokompanijaId = aviokompanijaId;
+	}
+
+	public Long getSjedisteId() {
+		return sjedisteId;
+	}
+
+	public void setSjedisteId(Long sjedisteId) {
+		this.sjedisteId = sjedisteId;
+	}
+
+	public Date getDatumPolaska() {
+		return datumPolaska;
+	}
+
+	public void setDatumPolaska(Date datumPolaska) {
+		this.datumPolaska = datumPolaska;
+	}
+
+	public Date getDatumDolaska() {
+		return datumDolaska;
+	}
+
+	public void setDatumDolaska(Date datumDolaska) {
+		this.datumDolaska = datumDolaska;
+	}
+
+	public double getCijena() {
+		return cijena;
+	}
+
+	public void setCijena(double cijena) {
+		this.cijena = cijena;
+	}
+
+	public int getProcenatPopusta() {
+		return procenatPopusta;
+	}
+
+	public void setProcenatPopusta(int procenatPopusta) {
+		this.procenatPopusta = procenatPopusta;
+	}
 
 }
--- src/main/java/rs/ac/uns/ftn/isa9/tim8/model/Segment.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/model/Segment.java
@@ -7,30 +7,41 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.ColumnDefault;
+
 @Entity
 @Table(name = "Segment")
 public class Segment {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long Id;
-	
+
 	@Column(name = "naziv")
 	protected String naziv;
-	
+
+	@Column(name = "cijena", nullable = false)
+	@ColumnDefault("0")
+	protected double cijena;
 
 	public Segment() {
-		this.naziv = "";
-	}
-
-	
-	
-	public Segment(String naziv) {
 		super();
-		this.naziv = naziv;
 	}
 
+	public Segment(Long id, String naziv, double cijena) {
+		super();
+		Id = id;
+		this.naziv = naziv;
+		this.cijena = cijena;
+	}
 
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
 
 	public String getNaziv() {
 		return naziv;
@@ -40,15 +51,12 @@ public class Segment {
 		this.naziv = naziv;
 	}
 
-
-	public Long getId() {
-		return Id;
+	public double getCijena() {
+		return cijena;
 	}
 
-	public void setId(Long id) {
-		Id = id;
+	public void setCijena(double cijena) {
+		this.cijena = cijena;
 	}
-	
-	
-	
+
 }
