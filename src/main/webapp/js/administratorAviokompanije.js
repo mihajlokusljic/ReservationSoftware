80024d53ce96a95f9555a3422f917ce73b42e376
Nikola Zubic <nikolazubic97@yahoo.com>
2019-06-11 15:22:57 +0200

    Zadavanje sjedista html/css dio

--- src/main/webapp/js/administratorAviokompanije.js
+++ src/main/webapp/js/administratorAviokompanije.js
@@ -512,54 +512,110 @@ $(document).ready(function() {
 	
 });
 
+function recalculateTotal(sc) {
+	  var total = 0;
+
+	  //basically find every selected seat and sum its price
+	  sc.find('selected').each(function () {
+	    total += this.data().price;
+	  });
+	  
+	  return total;
+	}
+
 function prikaziIzborSjedistaBrzeRezervacije() {
 	$("#izborLetaBrzeRezervacije").hide();
 	$("#izborSjedistaBrzeRez").show();
 	
-	var sc = $('#seat-map').seatCharts({
-		map: [
-			'aaaaaaaaaaaa',
-			'aaaaaaaaaaaa',
-			'bbbbbbbbbb__',
-			'bbbbbbbbbb__',
-			'bbbbbbbbbbbb',
-			'cccccccccccc'
-		],
-		seats: {
-			a: {
-				price   : 99.99,
-				classes : 'front-seat' //your custom CSS class
-			}
-		
-		},
-		click: function () {
-			if (this.status() == 'available') {
-				//do some stuff, i.e. add to the cart
-				return 'selected';
-			} else if (this.status() == 'selected') {
-				//seat has been vacated
-				return 'available';
-			} else if (this.status() == 'unavailable') {
-				//seat has been already booked
-				return 'unavailable';
-			} else {
-				return this.style();
-			}
-		}
-	});
+	firstSeatLabel = 1;
+    var $cart = $('#selected-seats'),
+    $counter = $('#counter'),
+    $total = $('#total'),
+    sc = $('#seat-map').seatCharts({
+    map: [
+      'ff_ff',
+      'ff_ff',
+      'ee_ee',
+      'ee_ee',
+      'ee___',
+      'ee_ee',
+      'ee_ee',
+      'ee_ee',
+      'eeeee',
+    ],
+    seats: {
+      f: {
+        price   : 100,
+        classes : 'first-class', //your custom CSS class
+        category: 'First Class'
+      },
+      e: {
+        price   : 40,
+        classes : 'economy-class', //your custom CSS class
+        category: 'Economy Class'
+      }         
+    
+    },
+    naming : {
+      top : false,
+      getLabel : function (character, row, column) {
+        return firstSeatLabel++;
+      },
+    },
+    legend : {
+      node : $('#legend'),
+        items : [
+        [ 'f', 'available',   'First Class' ],
+        [ 'e', 'available',   'Economy Class'],
+        [ 'f', 'unavailable', 'Already Booked']
+        ]         
+    },
+    click: function () {
+      if (this.status() == 'available') {
+        //let's create a new <li> which we'll add to the cart items
+        $('<li>'+this.data().category+' Seat # '+this.settings.label+': <b>$'+this.data().price+'</b> <a href="#" class="cancel-cart-item">[cancel]</a></li>')
+          .attr('id', 'cart-item-'+this.settings.id)
+          .data('seatId', this.settings.id)
+          .appendTo($cart);
+        
+        /*
+         * Lets up<a href="https://www.jqueryscript.net/time-clock/">date</a> the counter and total
+         *
+         * .find function will not find the current seat, because it will change its stauts only after return
+         * 'selected'. This is why we have to add 1 to the length and the current seat price to the total.
+         */
+        $counter.text(sc.find('selected').length+1);
+        $total.text(recalculateTotal(sc)+this.data().price);
+        
+        return 'selected';
+      } else if (this.status() == 'selected') {
+        //update the counter
+        $counter.text(sc.find('selected').length-1);
+        //and total
+        $total.text(recalculateTotal(sc)-this.data().price);
+      
+        //remove the item from our cart
+        $('#cart-item-'+this.settings.id).remove();
+      
+        //seat has been vacated
+        return 'available';
+      } else if (this.status() == 'unavailable') {
+        //seat has been already booked
+        return 'unavailable';
+      } else {
+        return this.style();
+      }
+    }
+  });
 
-	//Make all available 'c' seats unavailable
-	sc.find('c.available').status('unavailable');
-	
-	/*
-	Get seats with ids 2_6, 1_7 (more on ids later on),
-	put them in a jQuery set and change some css
-	*/
-	sc.get(['2_6', '1_7']).node().css({
-		color: '#ffcfcf'
-	});
-	
-	console.log('Seat 1_2 costs ' + sc.get('1_2').data().price + ' and is currently ' + sc.status('1_2'));
+  //this will handle "[cancel]" link clicks
+  $('#selected-seats').on('click', '.cancel-cart-item', function () {
+    //let's just trigger Click event on the appropriate seat, so we don't have to repeat the logic here
+    sc.get($(this).parents('li:first').data('seatId')).click();
+  });
+
+  //let's pretend some seats have already been booked
+  sc.get(['1_2', '4_1', '7_1', '7_2']).status('unavailable');
 }
 
 function pretraziSlobodneLetove() {
