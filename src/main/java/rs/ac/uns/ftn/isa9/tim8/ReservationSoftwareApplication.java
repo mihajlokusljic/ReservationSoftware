21fb46e0c96793094dbcdbbd81dcbcc6b86509ef
Nikola Zubic <nikolazubic97@yahoo.com>
2019-06-13 16:04:07 +0200

    Kompletno rjesen problem sa vremenima sinhronizacijom JVM

--- src/main/java/rs/ac/uns/ftn/isa9/tim8/ReservationSoftwareApplication.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/ReservationSoftwareApplication.java
@@ -1,5 +1,9 @@
 package rs.ac.uns.ftn.isa9.tim8;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,15 +12,21 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class ReservationSoftwareApplication {
 
+	@PostConstruct
+	static void started() {
+		// set JVM timezone as UTC
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 	public static void main(String[] args) {
+		started();
 		SpringApplication.run(ReservationSoftwareApplication.class, args);
 	}
 
 }
 
-
 /*
- 	Ukoliko je potreban drop baze radi promjene modela:
-spring.jpa.hibernate.ddl-auto=create-drop
-spring.datasource.initialization-mode=always
+ * Ukoliko je potreban drop baze radi promjene modela:
+ * spring.jpa.hibernate.ddl-auto=create-drop
+ * spring.datasource.initialization-mode=always
  */
