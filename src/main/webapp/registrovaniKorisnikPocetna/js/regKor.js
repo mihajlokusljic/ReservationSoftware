79a044994a454c6128817077411889bc68af3518
Nikola Zubic <nikolazubic97@yahoo.com>
2019-06-16 16:38:48 +0200

    Pretraga aviokompanija kod registrovanog korisnika

--- src/main/webapp/registrovaniKorisnikPocetna/js/regKor.js
+++ src/main/webapp/registrovaniKorisnikPocetna/js/regKor.js
@@ -17,6 +17,7 @@ var idLetaZaRezervaciju = -1;
 var podaciRezervacijeSjedista = null;
 var _podaciNeregistrovanihPutnika = [];
 var podaciBoravka = null;
+var defaultSlika = "https://cdn.logojoy.com/wp-content/uploads/2018/05/30142202/1_big-768x591.jpg";
 //  spring.datasource.initialization-mode=always
 
 $(document).ready(function() {	
@@ -597,7 +598,58 @@ $(document).ready(function() {
 		}
 	});
 	
+	$("#aviokompanijeSearchForm").submit(function(e) {
+		e.preventDefault();
+	   
+		let nazivAviokompanije = $("#aviokompanijaNazivSearch").val();
+		
+	
+		let pretragaAviokompanije = {
+				nazivAviokompanije: nazivAviokompanije
+		};
+		
+		$.ajax({
+			type: "POST",
+			url: "../aviokompanije/pretrazi/",
+			contentType : "application/json; charset=utf-8",
+			data: JSON.stringify(pretragaAviokompanije),
+			success: function(response) {
+				refreshAviokompanije(response);
+				if(response.length == 0) {
+					swal({
+						  title: "Ne postoji ni jedna aviokompanija koja zadovoljava navedeni kriterijum pretrage.",
+						  icon: "info",
+						  timer: 2500
+						});	
+				}
+				$('#aviokompanijeSearchForm')[0].reset();
+			}
+		});
+		
+	});
+	
 });
+
+function refreshAviokompanije(aviokompanije) {
+	let aviokompanijeTable = $("#prikazAviokompanija");
+	aviokompanijeTable.empty();
+	let aviokompanija = null;
+	for(var i in aviokompanije) {
+		aviokompanija = aviokompanije[i];
+		let noviRed = $("<tr></tr>");
+		noviRed.append('<td class="column1"><img src="' + defaultSlika + '"/></td>');
+		noviRed.append('<td class="column1">' + aviokompanija.naziv + "</td>");
+		noviRed.append('<td class="column1">' + aviokompanija.adresa.punaAdresa + "</td>");
+		if(aviokompanija.brojOcjena > 0) {
+			noviRed.append('<td class="column1">' + (aviokompanija.sumaOcjena / aviokompanija.brojOcjena) + "</td>");
+		} else {
+			noviRed.append('<td class="column1">0</td>');
+		}
+		noviRed.append('<td class="column1"><a href="../infoStranicaAviokompanije/index.html?id=' + 
+				aviokompanija.id + "&korisnik=" + korisnik.id +  '">Pogledaj detalje</a></td>');
+		aviokompanijeTable.append(noviRed);
+	}
+}
 
 function updateLetovi(letovi) {
 	let table = $("#prikazLetovaRezervacije");
