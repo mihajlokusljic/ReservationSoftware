b08f5d35bb5d4990fc99aeb6c29f81259c594ec9
Mihajlo Kusljic <mihajlokusljic97@gmail.com>
2019-06-19 00:01:33 +0200

    Implementirane transakcije rezervacije i brze rezervacije hotelske sobe

--- src/main/java/rs/ac/uns/ftn/isa9/tim8/repository/HotelskaSobaRepository.java
+++ src/main/java/rs/ac/uns/ftn/isa9/tim8/repository/HotelskaSobaRepository.java
@@ -2,7 +2,11 @@ package rs.ac.uns.ftn.isa9.tim8.repository;
 
 import java.util.Collection;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import rs.ac.uns.ftn.isa9.tim8.model.Hotel;
 import rs.ac.uns.ftn.isa9.tim8.model.HotelskaSoba;
@@ -10,5 +14,9 @@ import rs.ac.uns.ftn.isa9.tim8.model.HotelskaSoba;
 public interface HotelskaSobaRepository extends JpaRepository<HotelskaSoba, Long> {
 
 	public Collection<HotelskaSoba> findByHotel(Hotel hotel);
+	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT s FROM HotelskaSoba s WHERE s.id = :id")
+	HotelskaSoba zakljucajSobu(Long id);
 	
 }
